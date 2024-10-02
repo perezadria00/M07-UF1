bce82feba883c425b5cd71ea5a78ec9b7832a679
perezadria00 <pl2023275@365.stucom.com>
2024-10-02 19:32:05 +0200

    commit2

--- exercise02.php
+++ exercise02.php
@@ -1,6 +1,6 @@
 <?php
 
-/*1. Se declara una variable con un valor numérico. UƟlizando una estructura switch/case
+/*1. Se declara una variable con un valor numérico. Utilizando una estructura switch/case
 genera un mensaje que indique a qué día de la semana se corresponde.
 a. En caso de que no sea un valor entre el 1 y el 7 indica que no se corresponde
 con ningún día.  */
